464e87ce02e1b8ca23259db5042636feb9656c8c
nguyentin190301 <98431282+nguyentin190301@users.noreply.github.com>
2022-11-04 20:59:00 -0400

    Add files via upload

--- Data_Preprocessing_for_Topic_Models.py
+++ Data_Preprocessing_for_Topic_Models.py
@@ -12,6 +12,10 @@ import spacy
 
 from gensim import corpora
 
+from nltk.tokenize import word_tokenize
+import nltk
+nltk.download('punkt')
+
 def case_participation(filename, limit):
     """
     Make a dictionary author2doc of which cases (documents) each justice
@@ -30,17 +34,33 @@ def case_participation(filename, limit):
     return author2doc
 
 def clean_text(text):
+    #with open("stopwords_DF_threshold=500.txt", "r") as fp:
+    print('text:', text)
     with open("stop_words_Aug_01_2022.txt", "r") as fp:
-        stop_words = json.load(fp) 
+        stop_words = json.load(fp)
+    '''
+    stop_words_set = set(stop_words)
+
+    word_tokens = word_tokenize(text)
+    filtered_text = [w for w in word_tokens if len(w)>3 and (not any(char.isdigit() for char in w)) and (not w in stop_words_set)]
+
+    print('filtered_text:', filtered_text)
+    print('stop_words_set:', stop_words_set)
+    return filtered_text
+    '''
     delete_dict = {sp_character: '' for sp_character in string.punctuation} 
     delete_dict[' '] = ' ' 
     table = str.maketrans(delete_dict)
     text1 = text.translate(table)
     #print('cleaned:'+text1)
-    textArr= text1.lower().split()
-    text2 = ' '.join([w for w in textArr if ( not w.isdigit() and  ( not w.isdigit() and (not w in stop_words) and len(w)>3))]) 
-    
-    return text2
+    #textArr= text1.lower().split()
+    textArr = text1.split()
+    #delete tokens that contains number, tokens in stop word list, and too short tokens
+    text2 = ' '.join([w for w in textArr if (not any(char.isdigit() for char in w)) and (not w in stop_words) and len(w)>3])
+    print('text2:', text2)
+    return text2 
+
+
 
 
 def read_cases(filename, limit=-1):
