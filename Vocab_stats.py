dcd2910ea6aff9b0ce8553b4beb5182e5a35274b
nguyentin190301 <98431282+nguyentin190301@users.noreply.github.com>
2022-12-10 11:29:25 -0500

    Add files via upload

--- Vocab_stats.py
+++ Vocab_stats.py
@@ -2,13 +2,16 @@ import json
 import argparse
 from collections import Counter, defaultdict
 import numpy as np
+import csv
+import pandas as pd
+
 def Vocab_by_freq(file_name):
     with open(file_name, 'r') as f:
         read_cases_manualATM_text_list = json.load(f)
     read_cases_manualATM_text_list_flat = [item for sublist in read_cases_manualATM_text_list for item in sublist]
     #print('read_cases_manualATM_text_list_flat:', read_cases_manualATM_text_list_flat)
     Vocab = Counter(read_cases_manualATM_text_list_flat)
-    sorted_Vocab = {k: v for k, v in sorted(Vocab.items(), key=lambda item: item[1], reverse=True) if v > 3}
+    sorted_Vocab = {k: v for k, v in sorted(Vocab.items(), key=lambda item: item[1], reverse=True) if v > 0}
 
     tf_corpus_dict = {k: v/len(read_cases_manualATM_text_list_flat) for k, v in sorted_Vocab.items()}
     print("len(read_cases_manualATM_text_list_flat):", len(read_cases_manualATM_text_list_flat))
@@ -92,18 +95,23 @@ def Vocab_by_freq(file_name):
     #print('sorted_Vocab:', sorted_Vocab)
     print('len(sorted_Vocab):', len(sorted_Vocab))
 
-'''
-#with open('read_cases_manualATM_text_list_Dec04.json', 'r') as f:
-with open('read_cases_manualATM_text_list_Dec04.json', 'r') as f:
-    text_list = json.load(f)
 
-print("len(text_list):", len(text_list))
 
-#Initialize a Vocabulary dictionary with each preprocessed word as a key and
-#its corresponding corpus-level TF, DF, corpus-level TF-IDF, max doc-level TF,
-#max doc-level TF-IDF
-Vocacb_dict = {}
-'''
+def export_stats_to_csv():
+    with open('Vocab_stats_dict_Dec04.json', 'r') as f:
+      Vocab_stats_dict = json.load(f)
+
+    with open('Vocab_stats_Dec04.csv', 'w') as file:
+        writer = csv.writer(file)
+        writer.writerow(['word', 'corpus-level term freq', 'max doc-level term freq', 'inverse doc freq', 'corpus-level tf-idf', 'max doc-level tf-idf'])
+
+        for word, stats_list in Vocab_stats_dict.items():
+            row = [word] + stats_list
+            print('row:', row)
+            writer.writerow(row)
+
+        Vocab_stats_pd = pd.read_csv('Vocab_stats_Dec04.csv')
+    print("Number of lines :", len(Vocab_stats_pd))
 
 
 if __name__ == '__main__':
@@ -112,3 +120,4 @@ if __name__ == '__main__':
     flags = parser.parse_args()
 
     Vocab_by_freq(flags.file_name)
+    export_stats_to_csv()
