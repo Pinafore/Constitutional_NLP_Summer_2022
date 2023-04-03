85c81e6eea57298787023a714451486b5e78b837
nguyentin190301 <98431282+nguyentin190301@users.noreply.github.com>
2023-04-03 11:17:46 +0200

    Add files via upload

--- Calculate_coherence.py
+++ Calculate_coherence.py
@@ -19,7 +19,10 @@ def calculate_coherence(doc_words,topics,flag):
     for j in range(0,len(topics.keys())):
         score = 0
         if flag == 'AT':
-            topics_words = [topics[j][i][1] for i in range(0,len(topics[j]))]
+            #Just use the top 10 words per topic to calculate topic coherence
+            #topics_words = [topics[j][i][1] for i in range(0,len(topics[j]))]
+            topics_words = [topics[j][i] for i in range(0, 10)]
+            print('j:', j)
         if flag == 'LDA':
             topics_words = [topics[str(j)][i] for i in range(0,len(topics[str(j)]))]
         for pair in itertools.combinations_with_replacement(topics_words,2):
@@ -29,11 +32,13 @@ def calculate_coherence(doc_words,topics,flag):
             num_d_w1_w2 = d_w1_w2.count(True)
             score += math.log((num_d_w1_w2 + 1)/num_d_w1)
         score_dict[j] = score
+        print('score_dict:', score_dict)
     return score_dict
 
 
 if __name__ == "__main__":
-    with open('Data/read_cases_manualATM_text_list.json', 'r') as f:
+    '''
+    with open('Data/read_cases_manualATM_text_list_bverfg230107.json', 'r') as f:
         read_cases_manualATM_text_list = json.load(f)
     with open('Data/WardNJU_words_per_topic_num_topics=' + str(10) + '.json', 'rb') as f:
         topics_AT_10 = pickle.load(f)    
@@ -43,26 +48,33 @@ if __name__ == "__main__":
         topics_AT_100 = pickle.load(f)
     with open('Data/WardNJU_words_per_topic_num_topics=' + str(200) + '.json', 'rb') as f:
         topics_AT_200 = pickle.load(f)
-    with open('Data/LDA_formatted_top10_words_per_topic_num_topics=' + str(50) + '.json', 'rb') as f:
-        topics_LDA_50 = json.load(f)
+    #with open('Data/LDA_formatted_top10_words_per_topic_num_topics=' + str(50) + '.json', 'rb') as f:
+    #    topics_LDA_50 = json.load(f)
     
     score_dict_AT_10 = calculate_coherence(read_cases_manualATM_text_list,topics_AT_10,'AT')
     with open('Output/score_dict_AT_10.txt', 'w') as convert_file:
         convert_file.write(json.dumps(score_dict_AT_10))
     mean_score_dict_AT_10 = sum(score_dict_AT_10.values())/len(score_dict_AT_10.values())
     median_score_dict_AT_10 = median(score_dict_AT_10)
+    print('mean_score_dict_AT_10:', mean_score_dict_AT_10)
+    print('median_score_dict_AT_10:', median_score_dict_AT_10.values())
 
     score_dict_AT_50 = calculate_coherence(read_cases_manualATM_text_list,topics_AT_50,'AT')
     with open('Output/score_dict_AT_50.txt', 'w') as convert_file:
         convert_file.write(json.dumps(score_dict_AT_50))
     mean_score_dict_AT_50 = sum(score_dict_AT_50.values())/len(score_dict_AT_50.values())
     median_score_dict_AT_50 = median(score_dict_AT_50)
+    print('mean_score_dict_AT_50:', mean_score_dict_AT_50)
+    print('median_score_dict_AT_50:', median_score_dict_AT_50.values())
+
 
     score_dict_AT_100 = calculate_coherence(read_cases_manualATM_text_list,topics_AT_100,'AT')
     with open('Output/score_dict_AT_100.txt', 'w') as convert_file:
         convert_file.write(json.dumps(score_dict_AT_100))
     mean_score_dict_AT_100 = sum(score_dict_AT_100.values())/len(score_dict_AT_100.values())
     median_score_dict_AT_100 = median(score_dict_AT_100)
+    print('mean_score_dict_AT_100:', mean_score_dict_AT_100)
+    print('median_score_dict_AT_100:', median_score_dict_AT_100.values())
 
 
     score_dict_AT_200 = calculate_coherence(read_cases_manualATM_text_list,topics_AT_200,'AT')
@@ -70,24 +82,36 @@ if __name__ == "__main__":
         convert_file.write(json.dumps(score_dict_AT_200))
     mean_score_dict_AT_200 = sum(score_dict_AT_200.values())/len(score_dict_AT_200.values())
     median_score_dict_AT_200 = median(score_dict_AT_200)
+    print('mean_score_dict_AT_200:', mean_score_dict_AT_200)
+    print('median_score_dict_AT_200:', median_score_dict_AT_200.values())
 
     # print('score_dict_AT_50\n',score_dict_AT_50)
     # print('score_dict_AT_100\n',score_dict_AT_100)
     # print('score_dict_AT_200\n',score_dict_AT_200)
-    print('avaerage score_dict_AT\n', mean_score_dict_AT_10,mean_score_dict_AT_50,mean_score_dict_AT_100,mean_score_dict_AT_200)
-    print('meidan score_dict_AT\n', median_score_dict_AT_10, median_score_dict_AT_50,median_score_dict_AT_100,median_score_dict_AT_200)
-    score_dict_LDA = calculate_coherence(read_cases_manualATM_text_list,topics_LDA_50,'LDA')
-    with open('Output/score_dict_LDA.txt', 'w') as convert_file:
-        convert_file.write(json.dumps(score_dict_LDA))
-    print('score_dict_LDA\n',score_dict_LDA)
-    print('avaerage score_dict_LDA', sum(score_dict_LDA.values())/len(score_dict_LDA.values()))
+    print('average score_dict_AT\n', mean_score_dict_AT_10,mean_score_dict_AT_50,mean_score_dict_AT_100,mean_score_dict_AT_200)
+    print('median score_dict_AT\n', median_score_dict_AT_10, median_score_dict_AT_50,median_score_dict_AT_100,median_score_dict_AT_200)
 
+    mean_score_dict = {'10':mean_score_dict_AT_10, '50':mean_score_dict_AT_50, '100':mean_score_dict_AT_100, '200':mean_score_dict_AT_200}
+    median_score_dict = {'10':median_score_dict_AT_10, '50':median_score_dict_AT_50, '100':median_score_dict_AT_100, '200': median_score_dict_AT_200}
+    with open('Output/mean_score_dict.json', 'w') as convert_file:
+        convert_file.write(json.dumps(mean_score_dict))
+    #with open('Output/median_score_dict.json', 'w') as convert_file:
+    #    convert_file.write(json.dumps(median_score_dict))
+
+    #score_dict_LDA = calculate_coherence(read_cases_manualATM_text_list,topics_LDA_50,'LDA')
+    #with open('Output/score_dict_LDA.txt', 'w') as convert_file:
+    #    convert_file.write(json.dumps(score_dict_LDA))
+    #print('score_dict_LDA\n',score_dict_LDA)
+    #print('average score_dict_LDA', sum(score_dict_LDA.values())/len(score_dict_LDA.values()))
+
+    '''
     #df = pd.DataFrame(data = {'Number of topics': [50, 100, 200], 'Coherence score': [mean_score_dict_AT_50, mean_score_dict_AT_100,mean_score_dict_AT_200]})
-    df = pd.DataFrame(data = {'Number of topics': [10, 50, 100, 200], 'Average coherence score':[mean_score_dict_AT_10,-80.79144560414673,-95.62740792339578,-86.47907316046178]})
+    df = pd.DataFrame(data = {'Number of topics': [10, 50, 100, 200], 'Average coherence score':[-82.49657227363475, -80.65616921257829, -95.2023098845021, -89.25782542937264]})
     plt.plot(df['Number of topics'], df['Average coherence score'], marker="o")
     #sns.lineplot(data=df,x="Number of topics", y="Coherence score", markers=True, dashes=False)
-    plt.show()
-    df_median = pd.DataFrame(data = {'Number of topics': [10, 50, 100, 200], 'Median coherence score':[median_score_dict_AT_10,median_score_dict_AT_50,median_score_dict_AT_100,median_score_dict_AT_200]})
-    plt.plot(df_median['Number of topics'], df_median['Median coherence score'], marker="o")
-    plt.show()
+    plt.savefig("avg_coherence.png")
+    #plt.show()
+    #df_median = pd.DataFrame(data = {'Number of topics': [10, 50, 100, 200], 'Median coherence score':[median_score_dict_AT_10,median_score_dict_AT_50,median_score_dict_AT_100,median_score_dict_AT_200]})
+    #plt.plot(df_median['Number of topics'], df_median['Median coherence score'], marker="o")
+    #plt.show()
 
